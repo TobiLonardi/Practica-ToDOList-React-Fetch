e4ea3b1873ad71e8f1afe983db0f649d4c6488a1
Tobias Lonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-07-21 17:47:15 +0000

    layout listo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import React from "react";
 
 //include images into your bundle
@@ -5,22 +6,30 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [input,setInput]=useState("")
+	const handleSubmit=(event) =>{
+		event.preventDefault();
+		console.log(input)
+		setInput("")
+	}
+	return (
+		<div className="body d-flex flex-column justify-content-center align-items-center">
+			<h1>ToDoList</h1>
+			<form action="" className="w-50 shadow-sm p-1 mb-5 bg-body-tertiary rounded" onSubmit={handleSubmit}>
+				<div class="mb-3">
+					<input type="text" value={input}
+					 className="form-control "
+					  id="exampleInputEmail1"
+					   aria-describedby="emailHelp"
+					   onChange={(e) => setInput(e.target.value)}></input>
+					<div className="">
+						<p className="border-bottom m-1">cojerme a tu madre</p>
+						<p className="border-bottom m-1">cojerme a tu padre</p>
+
+					</div>
+				</div>
+			</form>
 		</div>
 	);
 };
