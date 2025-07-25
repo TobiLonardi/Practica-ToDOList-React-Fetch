ae6e129a6623cef3e7fcf5c6bf4e1faa1a6cd0f8
Tobias Lonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-07-25 16:41:30 +0000

    fetchs y apis añadidos

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import React from "react";
 
 //include images into your bundle
@@ -6,32 +6,114 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-
-	const [input,setInput]=useState("")
+	const api = "https://playground.4geeks.com/todo"
+	const [input,setInput]=useState({
+		"label":"",
+		"done":false
+	})
 	const [todos, setTodos] = useState([])
+	
+	const defaultValue={
+		"label":"",
+		"done":false
+	}
+
+	useEffect(()=>{
+		getAllTask()
+	},[])
+
 	const handleSubmit=(event) =>{
 		event.preventDefault();
-		setTodos([...todos,input])
-		setInput("")
+		//setTodos([...todos,input])
+		postToDo();
+		setInput(defaultValue)
 	}
-	const eliminateTask=(index)=>{
-		setTodos(todos.filter((tareas,tareaindex)=>tareaindex!==index))
-		console.log("borro")
+
+	const OnChange=(event)=>{
+		setInput({...input,
+			[event.target.name] : event.target.value
+			}
+		)
 	}
+
+	//const eliminateTask=(index)=>{
+	//	setTodos(todos.filter((tareas,tareaindex)=>tareaindex!==index))
+	//	console.log("borro")
+	//}
+
+	async function getAllTask() {
+		try {
+			const response = await fetch(`${api}/users/tobias`)
+			const data = await response.json()
+			if(response.ok){
+				setTodos(data.todos)
+			}
+			else if(response.status == 404)
+				createUser()
+
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	async function createUser() {
+		try {
+			const response = await fetch(`${api}/users/tobias`,{
+				method:"POST"
+			})
+			if(response.ok){
+				getAllTask()
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	async function postToDo(){
+		try {
+			const response = await fetch(`${api}/todos/tobias`,{
+				method:"POST",
+				headers:{
+					"Content-Type": "application/json"
+				},
+				body:JSON.stringify(input)
+			})
+			if(response.ok){
+				getAllTask();
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	async function deleteTask(id) {
+		try {
+			const response = await fetch(`${api}/todos/${id}`,{
+				method:"DELETE"
+			})
+			if(response.ok)
+				getAllTask()
+
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
 	return (
 		<div className="body d-flex flex-column justify-content-center align-items-center">
 			<h1>ToDoList</h1>
 			<form action="" className="w-50 shadow-sm p-1 mb-5 bg-body-tertiary rounded" onSubmit={handleSubmit}>
 				<div class="mb-3">
-					<input type="text" value={input}
+					<input type="text" value={input.label}
 					 className="form-control "
 					  id="exampleInputEmail1"
 					   aria-describedby="emailHelp"
-					   onChange={(e) => setInput(e.target.value)}></input>
+					   name = "label"
+					   onChange={OnChange}></input>
 					<ul className="">
-						{todos.map((tarea,index)=>
-						<li key={index} className="border-bottom m-1">{tarea}
-						<span onClick={()=> eliminateTask(index)}><i className="fa-solid fa-xmark"></i></span>
+						{todos.map((item)=>
+						<li key={item.id} className="border-bottom m-1">{item.label}
+						<span onClick={()=> deleteTask(item.id)}><i className="fa-solid fa-xmark"></i></span>
 						</li>)}
 
 					</ul>
